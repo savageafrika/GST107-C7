c368c9f81536d7de17c4587d0c7793134ccc30cd
savageafrika <113559289+savageafrika@users.noreply.github.com>
2023-11-20 07:08:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,395 @@
+
+
+
+
+ 
+
+   //Questions array
+
+   let questions = [
+
+    {
+
+         number:1,
+
+         question: "__ refers to communication by letters or emails exchanged",
+
+         answer : "Correspondence",
+
+         options : [
+
+                          "Business communication",
+
+                          "Public relation",
+
+                          "Circular",
+
+                          "Correspondence"
+
+                        ]
+
+    
+
+    },
+
+    {
+
+         number:2,
+
+         question: "All are forms of business correspondence except",
+
+         answer : "Financial statement",
+
+         options : [
+
+                          "Business letter",
+
+                          "Memorandum",
+
+                          "Financial statement",
+
+                          "Curriculum vitae"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:3,
+
+         question: "Which of these is not a part of a business letter",
+
+         answer : "Distribution list",
+
+         options : [
+
+                          "Name of the organisation",
+
+                          "Distribution list",
+
+                          "Postal address",
+
+                          "Reference number"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:4,
+
+         question: "letter writing, when the writer’s address is punctuated, it is referred to as ___ style",
+
+         answer : "Close style",
+
+         options : [
+
+                          "Open style",
+
+                          "Close style",
+
+                          "Indented style",
+
+                          "Block style"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:5,
+
+         question: "Salutation in business letters comes after __",
+
+         answer : "Recipient’s address",
+
+         options : [
+
+                          "Writer’s address",
+
+                          "Recipient’s address",
+
+                          "Heading",
+
+                          "Date"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:6,
+
+         question: "___ serves the purpose of the given in a nutshell what the letter is all about",
+
+         answer : "First paragraph",
+
+         options : [
+
+                          "Subject matter",
+
+                          "First paragraph",
+
+                          "Letterhead",
+
+                          "Title"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:7,
+
+         question: "The first paragraph of a letter is ___ in function",
+
+         answer : "Introductory",
+
+         options : [
+
+                          "Supporting",
+
+                          "Leading",
+
+                          "Introductory",
+
+                          "Expository"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:8,
+
+         question: "___ paragraph states the action required of the receiver",
+
+         answer : "Concluding paragraph",
+
+         options : [
+
+                          "First paragraph",
+
+                          "Second paragraph",
+
+                          "Concluding paragraph",
+
+                          "Middle paragraph"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:9,
+
+         question: "___ is used to indicate that the same letter has been produced and made available for persons occupying several offices",
+
+         answer : "Carbon copies",
+
+         options : [
+
+                          "Letter heading",
+
+                          "Postal address",
+
+                          "Enclosure",
+
+                          "Carbon copies"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:10,
+
+         question: "Where are extra documents attached to a letter written down",
+
+         answer : "Enclosure",
+
+         options : [
+
+                          "Letter heading",
+
+                          "Concluding paragraph",
+
+                          "Enclosure",
+
+                          "Carbon copies"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:11,
+
+         question: "Memorandum is __ type of business correspondence",
+
+         answer : "Internal correspondence",
+
+         options : [
+
+                          "Internal correspondence",
+
+                          "External correspondence",
+
+                          "Informal correspondence",
+
+                          "None of the above"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:12,
+
+         question: "Curriculum vitae is the Latin term meaning",
+
+         answer : "Course of one's life",
+
+         options : [
+
+                          "Business information",
+
+                          "Circle of life",
+
+                          "Course of one's life",
+
+                          "Personal statement"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:13,
+
+         question: "An alternative term for the curriculum vitae is",
+
+         answer : "Resume",
+
+         options : [
+
+                          "Memorandum",
+
+                          "Resume",
+
+                          "Business data",
+
+                          "Circular"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:14,
+
+         question: "At which section of the CV does a candidate need to state job or position he or she seeks to obtain",
+
+         answer : "Objectives",
+
+         options : [
+
+                          "Publication",
+
+                          "Personal data",
+
+                          "Working experience",
+
+                          "Objectives"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:15,
+
+         question: "The shortest segments of the CV is ___",
+
+         answer : "Extra curricular activities",
+
+         options : [
+
+                          "Additional skills",
+
+                          "Extra curricular activities",
+
+                          "Working experience",
+
+                          "Objectives"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:16,
+
+         question: "A person in a position to make reliable statements about a job candidate’s character, background and skills is called ____",
+
+         answer : "Referees",
+
+         options : [
+
+                          "Guarantor",
+
+                          "Referees",
+
+                          "Beneficiaries",
+
+                          "Job witness"  
+
+                          ]
+
+    
+
+    }
+
+     ];   
